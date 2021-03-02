887e25a5f60c0c04283f05049f65dd310c592da4
Eleanor <pyassistant00@gmail.com>
2021-03-02 01:40:15 -0500

    Added top users route

--- app/points/points.routes.js
+++ app/points/points.routes.js
@@ -9,4 +9,13 @@ router.get("/top-points", async (req, res) => {
   res.json(points);
 });
 
+router.get("/top-users/:userlimit?", async (req,res) => {
+  const userLimit = req.params.userlimit || 5;
+  const data = await topUserPoints();
+  const points = dataFormatter(data["users"], "username", "points");
+  const topUsers = Object.entries(points).slice(0,userLimit).map(entry => `${entry[0]}(${entry[1]})`)
+  const userList = topUsers.join(" , ");
+  res.send(userList);
+});
+
 module.exports = router;
